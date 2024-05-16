49fa833f5163777c2fb76e37df22471beb858661
Malcolm Scruggs <malcolm.scruggs@appfolio.com>
2024-05-16 16:39:14 -0700

    Repo error

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {useFonts} from "expo-font";
+
+export const fonts = {
+  'Helvetica Now Display': require('./assets/fonts/Helvetica Now Display.otf'),
+};
 
 export default function App() {
+  const [fontsLoaded, fontError] = useFonts(fonts);
+
+  if (fontError) {
+    console.warn('fontError', fontError);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +29,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontFamily: 'Helvetica Now Display',
+  }
 });
